5363152f50435d7a76cc0ae5628a91378000f270
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-05-26 23:55:21 +0200

    Declared variables in Global and Local Scopes

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
+// Global Scope
+var globalAge = 31;
+
+function greet() {
+  // Local Scope
+  var localName = 'Gaetan';
+  console.log('Hello ' + localName);
+}
+
+console.log(globalAge);
+
+greet();
+
 
