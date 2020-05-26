3430b685c1e325c3dadcc6e31ba27357635d801a
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-05-27 00:04:43 +0200

    Usage of  variables in Global and Local Scopes

--- app.js
+++ app.js
@@ -2,12 +2,24 @@
 var globalAge = 31;
 
 function greet() {
+
   // Local Scope
   var localName = 'Gaetan';
-  console.log('Hello ' + localName);
+
+  function printInfo() {
+    console.log('Hello ' + localName);
+    // Can access because in the Global Scope
+    console.log(`I am ${globalAge} years old`);
+
+  }
+
+  printInfo();
 }
 
 console.log(globalAge);
+
+// Reference Error because defined in the Local Scope
+// console.log(localName);
 
 greet();
 
