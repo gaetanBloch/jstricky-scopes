c51600ac78f9a96dd46e0acfdac71e9c0250052b
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-05-27 09:15:08 +0200

    Hoisting precedence

---
+++ hoisting/hoisting-precedence.js
@@ -0,0 +1,11 @@
+// Will use the username variable because it is defined
+// var username = 'Gaetan';
+
+// Will use the username function
+var username;
+
+function username() {
+  console.log('My username is Gaetan');
+}
+
+console.log(username);
