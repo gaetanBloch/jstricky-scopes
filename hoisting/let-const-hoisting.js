4d08e35696cc517b57aea4382b94e72593277fe6
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-05-27 09:09:01 +0200

    let and const hoisting

---
+++ hoisting/let-const-hoisting.js
@@ -0,0 +1,11 @@
+// Reference Error Cannot access before initialization
+// console.log(myName);
+let myName = 'Gaetan';
+console.log(myName);
+
+// Reference Error Cannot access before initialization
+// Cannot do the declaration and definition separately like with var
+// myAge = 31;
+// console.log(myAge);
+// let myAge;
+// console.log(myAge);
